78a638843a1c5fc175432083f462bbb7cc836488
burkayozbay <burkayozbayy@gmail.com>
2025-04-18 11:34:48 +0300

    Plus one text and coin image

---
+++ Assets/_app/Scripts/ClickSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ClickSpawner : MonoBehaviour
+{
+    public GameObject floatingEffectPrefab;
+    public Canvas floatingCanvas;
+    public Camera mainCamera;
+
+    public float spawnDistanceFromCamera = 1f; // z-position
+    public Vector3 effectWorldScale = new Vector3(0.01f, 0.01f, 0.01f); // small size
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 screenPos = Input.mousePosition;
+            screenPos.z = spawnDistanceFromCamera;
+
+            Vector3 worldPos = mainCamera.ScreenToWorldPoint(screenPos);
+
+            GameObject effect = Instantiate(floatingEffectPrefab, worldPos, Quaternion.identity, floatingCanvas.transform);
+
+            // 🔽 Scale it down for World Space
+            effect.transform.localScale = effectWorldScale;
+        }
+    }
+}
