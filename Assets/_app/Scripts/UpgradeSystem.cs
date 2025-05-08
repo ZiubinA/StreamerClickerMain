d7186344939443fe1992852706d5b21cd25055dc
CaIIister <tarashhh@gmail.com>
2025-05-09 02:50:45 +0300

    Implement apartment level upgrade system with visual changes

--- Assets/_app/Scripts/UpgradeSystem.cs
+++ Assets/_app/Scripts/UpgradeSystem.cs
@@ -33,9 +33,12 @@ public class UpgradeSystem : MonoBehaviour
     private CoinManager coinManager;
     private bool isPanelOpen = false;
 
+    private ApartmentManager apartmentManager;
+
     void Start()
     {
         coinManager = FindObjectOfType<CoinManager>();
+        apartmentManager = FindObjectOfType<ApartmentManager>();
 
         // Hide panels initially
         if (confirmationPanel != null)
@@ -237,6 +240,15 @@ public class UpgradeSystem : MonoBehaviour
         if (coinManager != null)
         {
             coinManager.ResetPlayerData();
+            // ADD THIS:
+            if (apartmentManager != null)
+            {
+                apartmentManager.ResetApartment();
+            }
+            else
+            {
+                Debug.LogError("ApartmentManager not found in UpgradeSystem!");
+            }
             UpdateUpgradeButtonTexts();
             CloseConfirmationPanel();
             CloseUpgradePanel();
